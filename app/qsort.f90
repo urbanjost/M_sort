492dcaa784f4e675bc3e19e2bfad5f9912e29e0c
John S. Urban <urbanjost@comcast.net>
2023-03-19 21:39:55 -0400

    SORT_QUICK_COMPACT(3f)

--- app/qsort.f90
+++ app/qsort.f90
@@ -1,5 +1,5 @@
 program qsort
-use M_io,      only : swallow
+use M_io,      only : fileread
 use M_CLI2,    only : set_args, lget , files=>unnamed
 use M_strings, only : upper, notabs, transliterate
 use M_sort,    only : sort_quick_rx
@@ -21,9 +21,9 @@ integer                            :: i, j, k, ilen, ios
       end select
    enddo
    do i=1,size(files)                                                            ! for each file read and reverse lines
-      call swallow(files(i),pageout)                                             ! allocate character array and copy file into it
+      call fileread(files(i),pageout)                                            ! allocate character array and copy file into it
       if(.not.allocated(pageout))then
-         write(*,*)'*demo_swallow* failed to load file '//files(i)
+         write(*,*)'*qsort* failed to load file '//files(i)
       else
          allocate(ii(size(pageout)))
          allocate(character(len=(len(pageout))) :: pageleft(size(pageout)))
@@ -44,37 +44,37 @@ subroutine setup()
 help_text=[ CHARACTER(LEN=128) :: &
 'NAME',&
 '       qsort(1f) - [FUNIX] sort a file',&
-'       (LICENSE:PD)',&
-'',&
-'SYNOPSIS',&
+'       (LICENSE:PD)                   ',&
+'                                      ',&
+'SYNOPSIS                              ',&
 '   qsort INPUT_FILE(S) [ --help][ --version]',&
-'',&
-'DESCRIPTION',&
+'                                            ',&
+'DESCRIPTION                                 ',&
 '   Sort lines in a file by reading into memory and sorting',&
 '   alphabetically. Case and non-alphanumeric characters are ignored.',&
-'',&
+'                                                                    ',&
 '   This is a simple use of the M_sort(3f) module and reads the files',&
-'   into memory, which could cause a machine to run out of memory if',&
-'   input files are large.',&
-'',&
-'OPTIONS',&
-'       INPUT_FILE(s)  input file(s)',&
-'       --help         display help text and exit',&
-'       --version      display version information and exit',&
-'',&
-'AUTHOR',&
-'   John S. Urban',&
-'',&
-'LICENSE',&
-'   Public Domain',&
+'   into memory, which could cause a machine to run out of memory if ',&
+'   input files are large.                                           ',&
+'                                                                    ',&
+'OPTIONS                                                             ',&
+'       INPUT_FILE(s)  input file(s)                                 ',&
+'       --help         display help text and exit                    ',&
+'       --version      display version information and exit          ',&
+'                                                                    ',&
+'AUTHOR                                                              ',&
+'   John S. Urban                                                    ',&
+'                                                                    ',&
+'LICENSE                                                             ',&
+'   Public Domain                                                    ',&
 '']
 version_text=[ CHARACTER(LEN=128) :: &
 'PRODUCT:        GPF (General Purpose Fortran) utilities and examples',&
-'PROGRAM:        qsort(1f)',&
-'DESCRIPTION:    sort lines in a file',&
-'VERSION:        1.0, 2021-01-10',&
-'AUTHOR:         John S. Urban',&
-'HOME PAGE:      http://www.urbanjost.altervista.org/index.html',&
+'PROGRAM:        qsort(1f)                                           ',&
+'DESCRIPTION:    sort lines in a file                                ',&
+'VERSION:        1.0, 2021-01-10                                     ',&
+'AUTHOR:         John S. Urban                                       ',&
+'HOME PAGE:      http://www.urbanjost.altervista.org/index.html      ',&
 '']
 end subroutine setup
 end program qsort
